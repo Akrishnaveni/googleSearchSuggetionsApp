7c7eb555a72545decb80a5f75de3d947a66d9aa1
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-11 19:09:17 +0530

    Update index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,12 +1,18 @@
 import 'index.css'
 
 const SuggetionItem = props => {
-  const {suggetion} = props
-
+  const {suggestionDetails} = props
+  const {id, suggestion} = suggestionDetails
   return (
     <li className="suggetion">
       <p className="description">{suggetion}</p>
-      <img src="" alt="arrow" className="image2" />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        className="image2"
+      />
     </li>
   )
 }
+
+export default SuggetionItem
