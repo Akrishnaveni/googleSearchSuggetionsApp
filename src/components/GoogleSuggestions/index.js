f500f496eac2b3a49e13dad6ea30f6902e9ac296
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-14 11:38:24 +0530

    Update index.js

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,72 +1,58 @@
-import {Component} from 'react'
-
-import SuggestionItem from '../SuggestionItem'
-
-import './index.css'
-
-class GoogleSuggestions extends Component {
-  state = {
-    searchInput: '',
-  }
-
-  updateSearchInput = value => {
-    this.setState({
-      searchInput: value,
-    })
-  }
 
   onChangeSearchInput = event => {
-    this.setState({
-      searchInput: event.target.value,
-    })
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteHistory = id => {
+    const {historyList} = this.state
+    const filterList = historyList.filter(each => each.id !== id)
+
+    this.setState({historyList: filterList})
   }
 
   render() {
-    const {searchInput} = this.state
-    const {suggestionsList} = this.props
-    const searchResults = suggestionsList.filter(eachSuggestion =>
-      eachSuggestion.suggestion
-        .toLowerCase()
-        .includes(searchInput.toLowerCase()),
+    const {historyList, searchInput} = this.state
+
+    const searchResult = historyList.filter(each =>
+      each.title.toLowerCase().includes(searchInput.toLowerCase()),
     )
 
     return (
       <div className="app-container">
-        <div className="google-suggestions-container">
+        <div className="header">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
-            alt="google logo"
-            className="google-logo"
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            className="history-logo"
+            alt="app logo"
           />
-          <div className="search-input-suggestions-container">
-            <div className="search-input-container">
-              <img
-                alt="search icon"
-                className="search-icon"
-                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
-              />
-              <input
-                type="search"
-                className="search-input"
-                placeholder="Search Google"
-                onChange={this.onChangeSearchInput}
-                value={searchInput}
-              />
-            </div>
-            <ul className="suggestions-list">
-              {searchResults.map(eachSuggestion => (
-                <SuggestionItem
-                  key={eachSuggestion.id}
-                  suggestionDetails={eachSuggestion}
-                  updateSearchInput={this.updateSearchInput}
-                />
-              ))}
-            </ul>
+          <div className="input-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+              className="search-image"
+              alt="search"
+            />
+            <input
+              placeholder="search History"
+              type="search"
+              value={searchInput}
+              className="input"
+              onChange={this.onChangeSearchInput}
+            />
           </div>
         </div>
+        <ul className="list-container">
+          {searchResult.map(each => (
+            <HistoryItem
+              historyDetails={each}
+              key={each.id}
+              deleteHistory={this.deleteHistory}
+              updateSearchInput={this.updateSearchInput}
+            />
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-export default GoogleSuggestions
+export default History
