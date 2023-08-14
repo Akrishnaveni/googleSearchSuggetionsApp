da6c90c15f2411269b3be0066cfcb6500664b979
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-14 09:45:22 +0530

    Update index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,18 +1,29 @@
 import './index.css'
 
-const SuggetionItem = props => {
-  const {suggestionDetails} = props
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
   const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
   return (
-    <li className="suggestion">
-      <p className="description">{suggestion}</p>
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
-        alt="arrow"
-        className="image2"
-      />
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
     </li>
   )
 }
 
-export default SuggetionItem
+export default SuggestionItem
